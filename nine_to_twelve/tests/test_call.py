302162d89958c2a5d6853e3e95bb1c05e5538b6d
Jeonghoon2 <ukidd12@naver.com>
2024-08-02 15:36:29 +0900

    Add Test and gitignore

--- nine_to_twelve/tests/test_call.py
+++ nine_to_twelve/tests/test_call.py
@@ -1,6 +1,12 @@
-from nine_to_twelve.api.ninetotwelve import ice_breaking
+from nine_to_twelve.api.ninetotwelve import ice_breaking, req
 
 def test_ice_breaking():
     
     assert ice_breaking() == "ice_breaking"
     assert True
+
+def test_data_check():
+    df = req()
+    
+    assert len(df) > 1
+    
